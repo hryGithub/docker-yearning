28ce09fa17dd767d4b8392bf4dc5c813418c3328
hyr <516870171@qq.com>
2021-07-13 10:03:37 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+sed -i 's@Db = "Yearning"@Db = "$MYSQL_DB"@g' conf.toml
+sed -i 's@Host = "127.0.0.1"@Host = "$MYSQL_ADDR"@g' conf.toml
+sed -i 's@Port = "3306"@Port = "$MYSQL_PORT"@g' conf.toml
+sed -i 's@Password = ""@Password = "$MYSQL_PASSWORD"@g' conf.toml
+sed -i 's@User = "root"@User = "$MYSQL_USER"@g' conf.toml
 ./Yearning install && ./Yearning run 
